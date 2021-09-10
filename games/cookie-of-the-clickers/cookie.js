04b626530c2a2e0391e35faf780eb67497a150b0
Sayo-Exo <75951527+Sayo-Exo@users.noreply.github.com>
2021-09-10 15:52:51 -0400

    Update cookie.js

--- games/cookie-of-the-clickers/cookie.js
+++ games/cookie-of-the-clickers/cookie.js
@@ -1,4 +1,4 @@
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exdays = 10000000000) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   let expires = "expires="+ d.toUTCString();
