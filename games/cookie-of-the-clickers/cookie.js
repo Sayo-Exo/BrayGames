ce7773e75e7a57266c8503de640c94609a56dd87
Sayo-Exo <75951527+Sayo-Exo@users.noreply.github.com>
2021-09-10 15:43:37 -0400

    Update cookie.js

--- games/cookie-of-the-clickers/cookie.js
+++ games/cookie-of-the-clickers/cookie.js
@@ -1,5 +1,29 @@
-//initial number of cookies    
-var num = 0;
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+if (getCookie("cookies") == null)
+    setCookie("cookies", 0);
+var num = getCookie("cookies");
 
 window.onload = function () {
         var name = prompt("What is your name");
