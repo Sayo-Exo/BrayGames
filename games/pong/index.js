c4c1f9ca2c09ebe559f6e45bcd00fc1e7cd22707
Sayo-Exo <75951527+Sayo-Exo@users.noreply.github.com>
2021-09-23 11:39:30 -0400

    Update index.js

--- games/pong/index.js
+++ games/pong/index.js
@@ -1,3 +1,15 @@
+(function () {
+    var old = console.log;
+    var logger = document.getElementById('log');
+    console.log = function (message) {
+        if (typeof message == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+        } else {
+            logger.innerHTML += message + '<br />';
+        }
+    }
+})();
+
 let gameState = 'start';
 var multiplayer = false;
 if (confirm("Multiplayer?"))
