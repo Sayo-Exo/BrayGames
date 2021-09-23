97450fbdd7dd8411611600faef10f280d9f97433
Sayo-Exo <75951527+Sayo-Exo@users.noreply.github.com>
2021-09-23 11:46:07 -0400

    logger fixes

--- games/pong/index.js
+++ games/pong/index.js
@@ -4,11 +4,13 @@ if (!console) {
 var old = console.log;
 var logger = document.getElementById('log');
 console.log = function (message) {
-if (typeof message == 'object') {
-	logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
-} else {
-	logger.innerHTML += message + '<br />';
+	if (typeof message == 'object') {
+		logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
+	} else {
+		logger.innerHTML += message + '<br />';
+	}
 }
+console.log("Started!");
 
 let gameState = 'start';
 var multiplayer = false;
