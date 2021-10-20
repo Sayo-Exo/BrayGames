d623aff0d9aa2ac52121ff6447d9144979fdd95a
Sayo-Exo <75951527+Sayo-Exo@users.noreply.github.com>
2021-10-20 11:44:12 -0400

    Update script.js

--- accounts/script.js
+++ accounts/script.js
@@ -1,3 +1,13 @@
+function setCookie(cname, cvalue, exdays=null) {
+  var expires = "";
+  if (exdays != null) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    expires = "expires="+d.toUTCString() + ";";
+  }
+  document.cookie = cname + "=" + cvalue + ";" + expires + "path=/";
+}
+
 function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
@@ -17,10 +27,14 @@ function getCookie(cname) {
 usernameTXT = document.getElementById("username");
 function GetUsername() {
     if (getCookie("username") == "") {
-        usernameTXT.innerHTML = "(not logged in)";
+        SetUsername();
     } else
     {
         usernameTXT.innerHTML = "Username: " + getCookie("username");
     }
+}
+function SetUsername()
+{
+    
 }
 username = GetUsername();
